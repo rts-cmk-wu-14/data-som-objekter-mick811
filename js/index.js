a437587eb9366d0acdba80c6686024e2a8105d4d
mick811 <mikkelelvers@outlook.com>
2025-08-27 09:21:19 +0200

    added small util functions

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
 /**
- * @param {Hero} hero
- * @param {Service[]} services
- * @param {Facilities} facilities
- * @param {Sites} sites
- * @param {Advantage[]} advantages
-*/
+ * @param {string} type
+ * @param {Object} attributes
+ * @param {string} textContent
+ * @returns {HTMLElement}
+ */
+function createElement(type, attributes = {}, textContent = '') {
+    const element = document.createElement(type);
+    Object.keys(attributes).forEach(key => {
+        element.setAttribute(key, attributes[key]);
+    });
+    if (textContent) element.textContent = textContent;
+    return element;
+}
+
+/**
+ * Wraps specific text phrases in spans for styling
+ * @param {string} text
+ * @param {string[]} phrases
+ * @returns {string}
+ */
+function wrapTextPhrases(text, phrases) {
+    let result = text;
+    phrases.forEach(phrase => {
+        const regex = new RegExp(`(${phrase})`, 'gi');
+        result = result.replace(regex, '<span class="highlight-text">$1</span>');
+    });
+    return result;
+}
