0fcbcc8318c9df755068bd20a9e33bcd7a6f329e
mick811 <mikkelelvers@outlook.com>
2025-08-27 09:22:20 +0200

    added hero section

--- js/index.js
+++ js/index.js
@@ -27,3 +27,39 @@ function wrapTextPhrases(text, phrases) {
     });
     return result;
 }
+
+
+/**
+ * Renders the hero section
+ * @param {Hero} heroData
+ */
+function renderHero(heroData) {
+    const section = document.querySelector('.hero');
+    if (!section) return;
+    
+    const heroContainer = createElement('section', { class: 'hero-container' });
+    const heroImage = createElement('img', { src: heroData.image, alt: heroData.headline, class: 'hero-image' });
+    const heroBox = createElement('div', { class: 'hero-box' });
+    const heroContent = createElement('article', { class: 'hero-content' });
+    const heroHeadline = createElement('h1', { class: 'hero-headline' });
+    
+    // Wrap specific phrases in spans for styling
+    const highlightPhrases = ['save your time'];
+    heroHeadline.innerHTML = wrapTextPhrases(heroData.headline, highlightPhrases);
+    
+    const heroCopy = createElement('p', { class: 'hero-copy' }, heroData.copy);
+    const heroIconWrapper = createElement('div', { class: 'hero-icon' });
+    const heroIconImg = createElement('img', { src: heroData.icon, alt: 'Hero icon' });
+    heroIconWrapper.appendChild(heroIconImg);
+
+    heroBox.appendChild(heroContent);
+    
+    heroContent.appendChild(heroHeadline);
+    heroContent.appendChild(heroCopy);
+    heroContent.appendChild(heroIconWrapper);
+    heroContainer.appendChild(heroImage);
+    heroContainer.appendChild(heroBox);
+    section.appendChild(heroContainer);
+}
+
+renderHero(hero)
